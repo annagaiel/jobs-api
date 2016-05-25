157e46c79daea2f246c68eb5ade83ee839b0b1a7
Anna Natorilla <annagaiel@gmail.com>
2016-05-24 20:28:24 -0700

    Add jobs controller

--- app/assets/javascripts/jobsCtrl.js
+++ app/assets/javascripts/jobsCtrl.js
@@ -2,7 +2,11 @@
   "use Strict"
   angular.module("app").controller("jobsCtrl",
     function($scope, $http){
-
+      $scope.setup = function(){
+        $http.get("/api/v1/jobs.json").then(function(response){
+          $scope.jobs = response.data;
+        });
+      }
       window.$scope = $scope;
   });
 })();
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'jobs/index'
+
   namespace :api do
     namespace :v1 do
       resources :jobs
